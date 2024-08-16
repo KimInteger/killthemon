7be370210ea2e14caec51ce85cc85c95918fc9d6
김정수 <kjs20151240@gmail.com>
2024-08-16 14:58:37 +0900

    edit | server/app/main.py
    log를 남기기위한 logging import index.html서빙

--- server/app/main.py
+++ server/app/main.py
@@ -1,18 +1,32 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
 
 env_path = Path(__file__).resolve().parent.parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
+
+index_html_path = Path(__file__).resolve().parent.parent.parent / 'index.html'
 
 app = FastAPI()
 
 PORT = int(os.getenv("CLI_PORT"))
 
 @app.get("/")
-def hello() :
-    print("hellow wolrd")
+async def hello() :
+    try : 
+        logger.info("serving index.html")
+        with open(index_html_path, 'r') as file :
+            return HTMLResponse(content=file.read(), status_code=200)
+    except Exception as e:
+        logger.error(f"Error serving index.html : {str(e)}")
+        return HTMLResponse(content=f"Error: {str(e)}", status_code=500)
 
 
 if __name__ == '__main__' :
