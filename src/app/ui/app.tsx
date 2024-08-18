510cd81c1989bcd2a259c9ee48e850fef6d9de1c
김정수 <kjs20151240@gmail.com>
2024-08-18 16:07:26 +0900

    edit | app.tsx
    랜더링 체크

--- src/app/ui/app.tsx
+++ src/app/ui/app.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PlayerSection from './player/playerSection';
 
 const App: React.FC = () => {
+  const [player, setPlayer] = useState({
+    name: 'player',
+    hp: 100,
+    atk: 20,
+  });
+
+  const [monster, setMonster] = useState({
+    name: 'monster',
+    hp: 200,
+    def: 10,
+  });
+
+  function attack() {
+    setMonster((prevMonster) => ({
+      ...prevMonster,
+      hp: prevMonster.hp - Math.max(player.atk - prevMonster.def, 0),
+    }));
+  }
+
   return (
     <>
       <h1>몬스터를 죽여라!</h1>
+      <PlayerSection player={player} attack={attack} />
     </>
   );
 };
