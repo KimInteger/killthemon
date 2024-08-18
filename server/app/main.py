de651b61f1367d71f7960efa92933270c95ab888
김정수 <kjs20151240@gmail.com>
2024-08-18 12:55:05 +0900

    add | server/app/main.py
    index.bundle.js 요청 처리

--- server/app/main.py
+++ server/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse, PlainTextResponse
 from dotenv import load_dotenv
 import os
 from pathlib import Path
@@ -13,6 +13,7 @@ env_path = Path(__file__).resolve().parent.parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
 index_html_path = Path(__file__).resolve().parent.parent.parent / 'index.html'
+bundle_js_path = Path(__file__).resolve().parent.parent.parent / 'dist/index.bundle.js'
 
 app = FastAPI()
 
@@ -27,6 +28,15 @@ async def hello() :
     except Exception as e:
         logger.error(f"Error serving index.html : {str(e)}")
         return HTMLResponse(content=f"Error: {str(e)}", status_code=500)
+
+@app.get("/dist/index.bundle.js")
+async def getdist() :
+    try : 
+        return FileResponse(path=bundle_js_path, media_type="application/javascript")
+    except Exception as e:
+        logger.error(f"Error serving index.html : {str(e)}")
+        return PlainTextResponse(content=f"Error: {str(e)}", status_code=500)
+    
 
 
 if __name__ == '__main__' :
