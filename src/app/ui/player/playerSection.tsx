b4a88243d157da4b86646133150c8f1a7c94777b
김정수 <kjs20151240@gmail.com>
2024-08-18 16:07:09 +0900

    create | playerSection.tsx
    player섹션 만들기

--- src/app/ui/player/playerSection.tsx
+++ src/app/ui/player/playerSection.tsx
@@ -1,3 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const playerSection = () => {};
+type PlayerInfo = {
+  name: string;
+  hp: number;
+  atk: number;
+};
+
+interface PlayerProps {
+  player: PlayerInfo;
+  attack: () => void;
+}
+
+const PlayerSection: React.FC<PlayerProps> = ({ player, attack }) => {
+  return (
+    <div>
+      <h2>{player.name}</h2>
+      <p>{player.hp}</p>
+      <p>{player.atk}</p>
+      <button onClick={attack}>공격!!</button>
+    </div>
+  );
+};
+
+export default PlayerSection;
